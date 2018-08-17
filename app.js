ddfb9891c9669f52ecbbe3dbb2b8701958e03821
Sam Meech-Ward <sam@meech-ward.com>
2018-08-17 12:52:43 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const onoff = require('onoff');
+const Gpio = onoff.Gpio;
+
+const led = new Gpio(17, 'out');
+const sensor = new Gpio(21, 'in', 'both');
+ 
+sensor.watch((err, value) => {
+  if (err) {
+    console.log("ERror", err);
+    return;
+  }
+  console.log("State", value);
+  led.writeSync(value);
+});
